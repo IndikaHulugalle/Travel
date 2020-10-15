dfd993bcb7a50830e013f332c5193fd1298bf8b4
INDIKA <hulugalleindika@gmail.com>
2020-10-15 16:30:44 +0530

    Initial commit

---
+++ app/src/main/java/com/travel/Feedback.java
@@ -0,0 +1,34 @@
+package com.travel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+
+
+
+
+        Button back = findViewById(R.id.Btnvback);
+
+
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Feedback.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
